8ce108be9a5149ebfdd212446d80f88636e343dd
Han-Lon <josephmaxmorris7@gmail.com>
2022-09-04 20:33:29 -0500

    Add comments to the ec2-instance and server-bootstrap code

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -14,6 +14,7 @@ data "aws_ami" "ubuntu-ami" {
   }
 }
 
+# TODO put this in SSM Param Store
 resource "random_password" "initial-user-password" {
   length = 30
 }
--- server-bootstrap.sh
+++ server-bootstrap.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
+# Restore default APT repositories for Ubuntu 20.04 Focal -- per this SO thread https://askubuntu.com/a/1367041
+cat <<EOF | sudo tee /etc/apt/sources.list
+deb http://archive.ubuntu.com/ubuntu/ focal main universe multiverse restricted
+deb http://security.ubuntu.com/ubuntu/ focal-security main universe multiverse restricted
+deb http://archive.ubuntu.com/ubuntu/ focal-updates main universe multiverse restricted
+deb http://archive.ubuntu.com/ubuntu/ focal-backports main universe multiverse restricted
+
+deb-src http://archive.ubuntu.com/ubuntu/ focal main universe multiverse restricted
+deb-src http://security.ubuntu.com/ubuntu/ focal-security main universe multiverse restricted
+deb-src http://archive.ubuntu.com/ubuntu/ focal-updates main universe multiverse restricted
+deb-src http://archive.ubuntu.com/ubuntu/ focal-backports main universe multiverse restricted
+EOF
+
+# TODO try this instead, might be better than the multiline above
+add-apt-repository universe
+
 export DEBIAN_FRONTEND=noninteractive
+dpkg --add-architecture i386
 apt-get update -y && apt-get -o "Dpkg::Options::=--force-confold" dist-upgrade -y --force-yes
 
-useradd -p $(openssl passwd -crypt "${PASSWD}") bf1942server
+apt-get install binutils jq lib32gcc1 lib32stdc++6 libncurses5:i386 libsdl2-2.0-0:i386 libtinfo5:i386 netcat unzip -y
+
+useradd -p $(openssl passwd -crypt "${PASSWD}") -m -s /bin/bash bf1942server
 
 sudo -u bf1942server -i << EOF
 
-wget -O ~/linuxgsm.sh https://linuxgsm.sh && chmod +x ~/linuxgsm.sh && bash ~/linuxgsm.sh bf1942server
-~/bf1942server install
+wget -O ~/linuxgsm.sh https://linuxgsm.sh \
+&& chmod +x ~/linuxgsm.sh \
+&& bash ~/linuxgsm.sh bf1942server \
+&& /usr/bin/yes | ~/bf1942server install
+
 EOF

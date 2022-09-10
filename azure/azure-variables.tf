177e7c139c881274832ff1790945e50700d629e4
Han-Lon <josephmaxmorris7@gmail.com>
2022-09-10 13:55:43 -0500

    Oopsie, I read my own code too fast lol. Actual fix for Azure SSH security group

--- azure/azure-variables.tf
+++ azure/azure-variables.tf
@@ -1,6 +1,6 @@
 variable "allowed-ssh-ip" {
   description = "Optional: The public IP address to allow SSH traffic. Highly recommended to set this as you REALLY want to restrict SSH traffic to your own IP."
-  default = null
+  default = ""
   type = string
 }
 
